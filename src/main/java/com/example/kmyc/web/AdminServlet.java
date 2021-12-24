09af5b02ee1d6391ab967a5f5ab52e39dbd37b0c
“xinqiyi1024” <3633414863@qq.com>
2021-12-24 19:31:43 +0800

    updateServlet

--- src/main/java/com/example/kmyc/web/AdminServlet.java
+++ src/main/java/com/example/kmyc/web/AdminServlet.java
@@ -9,7 +9,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
@@ -27,42 +26,30 @@ import java.util.Map;
 public class AdminServlet extends BaseServlet {
 
     private AdminService adminService = new AdminServiceImpl();
+    Map<String, Object> info = null;
 
-    public void register(HttpServletRequest request, HttpServletResponse response) throws  IOException {
+    public void register(HttpServletRequest request, HttpServletResponse response) throws IOException {
         // 接收参数 { name:? ; password:? }
-        // 获取字符流 读取类
-        BufferedReader reader = request.getReader();
-        // 反序列化，把json数据转换成object
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String,Object> admin = mapper.readValue(reader,Map.class);
-        System.out.println(admin);
+        info = getParam4Service(request);
         // 调用业务层方法来录入数据
-        Boolean flag = adminService.adminRegister((String) admin.get("name"),
-                (String)admin.get("password"));
-        Result result = new Result(200,flag,"/register");
-        // 将输出结果序列化
-        String json = mapper.writeValueAsString(result);
-        // 输出结果
-        response.getWriter().print(json);
+        Boolean flag = adminService.adminRegister((String) info.get("name"),
+                (String) info.get("password"));
+        Result result = new Result(200, flag, "/register");
+        // 序列化,并输出
+        mapper4Json(response, result);
     }
+
     public void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 接收参数 { name:? ; password:? }
-        // 获取字符流 读取类
-        BufferedReader reader = request.getReader();
-        // 定义线程可变的字符串
-        // 反序列化，把json数据转换成object
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String,Object> admin = mapper.readValue(reader,Map.class);
-        System.out.println(admin);
+        info = getParam4Service(request);
         // 调用业务层方法来录入数据
-        Boolean flag = adminService.adminLogin((String) admin.get("name"),
-                (String)admin.get("password"));
-        Result result = new Result(200,flag,"/login");
-        // 将输出结果序列化
-        String json = mapper.writeValueAsString(result);
-        // 输出结果
-        response.getWriter().print(json);
+        Boolean flag = adminService.adminLogin((String) info.get("name"),
+                (String) info.get("password"));
+        Result result = new Result(200, flag, "/login");
+        // 序列化
+        mapper4Json(response, result);
     }
+
     public void deleteAdmin(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 接收参数
         // 获取字符流 读取类
@@ -70,16 +57,17 @@ public class AdminServlet extends BaseServlet {
         // 定义线程可变的字符串
         // 反序列化，把json数据转换成object
         ObjectMapper mapper = new ObjectMapper();
-        Map<String,Object> admin = mapper.readValue(reader,Map.class);
+        Map<String, Object> admin = mapper.readValue(reader, Map.class);
         System.out.println(admin);
         // 调用业务层方法来录入数据
         Boolean flag = adminService.deleteAdmin(Long.valueOf(admin.get("id").toString()));
-        Result result = new Result(200,flag,"/deleteAdmin");
+        Result result = new Result(200, flag, "/deleteAdmin");
         // 将输出结果序列化
         String json = mapper.writeValueAsString(result);
         // 输出结果
         response.getWriter().print(json);
     }
+
     public void recover(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 接收参数
         // 获取字符流 读取类
@@ -87,16 +75,17 @@ public class AdminServlet extends BaseServlet {
         // 定义线程可变的字符串
         // 反序列化，把json数据转换成object
         ObjectMapper mapper = new ObjectMapper();
-        Map<String,Object> admin = mapper.readValue(reader,Map.class);
+        Map<String, Object> admin = mapper.readValue(reader, Map.class);
         System.out.println(admin);
         // 调用业务层方法来录入数据
         Boolean flag = adminService.recoverAdmin(Long.valueOf(admin.get("id").toString()));
-        Result result = new Result(200,flag,"/recover");
+        Result result = new Result(200, flag, "/recover");
         // 将输出结果序列化
         String json = mapper.writeValueAsString(result);
         // 输出结果
         response.getWriter().print(json);
     }
+
     public void updateRole(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 接收参数
         // 获取字符流 读取类
@@ -104,16 +93,17 @@ public class AdminServlet extends BaseServlet {
         // 定义线程可变的字符串
         // 反序列化，把json数据转换成object
         ObjectMapper mapper = new ObjectMapper();
-        Map<String,Object> admin = mapper.readValue(reader,Map.class);
+        Map<String, Object> admin = mapper.readValue(reader, Map.class);
         System.out.println(admin);
         // 调用业务层方法来录入数据
-        Boolean flag = adminService.adminUpdateRole( Long.parseLong(admin.get("id").toString()),(String) admin.get("role"));
-        Result result = new Result(200,flag,"/updateRole");
+        Boolean flag = adminService.adminUpdateRole(Long.parseLong(admin.get("id").toString()), (String) admin.get("role"));
+        Result result = new Result(200, flag, "/updateRole");
         // 将输出结果序列化
         String json = mapper.writeValueAsString(result);
         // 输出结果
         response.getWriter().print(json);
     }
+
     public void updatePassword(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 接收参数
         // 获取字符流 读取类
@@ -121,16 +111,17 @@ public class AdminServlet extends BaseServlet {
         // 定义线程可变的字符串
         // 反序列化，把json数据转换成object
         ObjectMapper mapper = new ObjectMapper();
-        Map<String,Object> admin = mapper.readValue(reader,Map.class);
+        Map<String, Object> admin = mapper.readValue(reader, Map.class);
         System.out.println(admin);
         // 调用业务层方法来录入数据
-        Boolean flag = adminService.adminUpdatePassword(Long.valueOf(admin.get("id").toString()),(String) admin.get("oldPassword"),(String) admin.get("newPassword"));
-        Result result = new Result(200,flag,"/updatePassword");
+        Boolean flag = adminService.adminUpdatePassword(Long.valueOf(admin.get("id").toString()), (String) admin.get("oldPassword"), (String) admin.get("newPassword"));
+        Result result = new Result(200, flag, "/updatePassword");
         // 将输出结果序列化
         String json = mapper.writeValueAsString(result);
         // 输出结果
         response.getWriter().print(json);
     }
+
     public void listAdmin(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 接收参数
         // 获取字符流 读取类
@@ -140,7 +131,7 @@ public class AdminServlet extends BaseServlet {
         ObjectMapper mapper = new ObjectMapper();
         // 调用业务层方法来查询数据
         List<Admin> admins = adminService.listAdmin();
-        Result result = new Result(200,admins,"/listAdmin");
+        Result result = new Result(200, admins, "/listAdmin");
         // 将输出结果序列化
         String json = mapper.writeValueAsString(result);
         // 输出结果
--- src/main/java/com/example/kmyc/web/base/BaseServlet.java
+++ src/main/java/com/example/kmyc/web/base/BaseServlet.java
@@ -1,6 +1,9 @@
 package com.example.kmyc.web.base;
 
-import com.example.kmyc.web.AdminServlet;
+import com.alibaba.fastjson.JSON;
+import com.example.kmyc.result.Result;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 1.*快乐每一天
@@ -18,26 +23,58 @@ import java.lang.reflect.Method;
 public class BaseServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/json;charset=UTF-8");
-        resp.setCharacterEncoding("utf-8");
-        System.out.println("BaseServlet 的 service方法执行了------");
-        // 获取请求路径
-        String uri = req.getRequestURI();// 请求资源标识符，范围更大
-        StringBuffer url = req.getRequestURL();// 请求资源定位符
-        System.out.println(uri+","+url);
-        // 2.获取方法名称
-        String methodName = uri.substring(uri.lastIndexOf("/") + 1);
-        System.out.println(methodName);
+        // 解决乱码
+        resp.setHeader("content-type", "text/html; charset=UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        req.setCharacterEncoding("UTF-8");
+        // 1.获取请求路径,请求资源标识符
+        String uri = req.getRequestURI();
+        // 请求资源定位符
+        StringBuffer url = req.getRequestURL();
 
-        // 3.获取方法要执行的对象
-        System.out.println(this);
+        // 2.获取方法名称
+        String methodName = url.substring(url.lastIndexOf("/") + 1);
+
+        // 3.获取this
         try {
-            // 4.先获取当前对象的字节码然后获取一个公开的方法
-            Method method = this.getClass().getMethod(methodName,HttpServletRequest.class,HttpServletResponse.class);
+            // 4.先当前对象的字节码，然后获取一个公开的方法
+            Method method = this.getClass().getMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
             // 5.执行方法
-            method.invoke(this,req,resp);
+            method.invoke(this, req, resp);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static Map<String, Object> getParam4Service(HttpServletRequest request) {
+        Map<String, Object> params = new HashMap<>();
+        String format = "application/json";
+
+        // 获取内容格式
+        String contentType = request.getContentType();
+        if (contentType != null && !"".equals(contentType)) {
+            contentType = contentType.split(",")[0];
+        }
+        // json格式 json格式需要从request的输入流中解析获取
+        if (format.equalsIgnoreCase(contentType)) {
+            // 使用 commons-io中 IOUtils 类快速获取输入流内容
+            String paramJson = null;
+            try {
+                paramJson = IOUtils.toString(request.getInputStream(), "UTF-8");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Map parseObject = JSON.parseObject(paramJson, Map.class);
+            params.putAll(parseObject);
+        }
+        return params;
+    }
+
+    public void mapper4Json(HttpServletResponse resp, Result result) throws IOException {
+        // 将结果序列化
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(result);
+        // 输出结果
+        resp.getWriter().print(json);
     }
 }
